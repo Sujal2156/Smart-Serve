355c2e95fcabd0bdb01a5f932f9428d775e8b395
Sujal Bhalala <156934445+Sujal2156@users.noreply.github.com>
2026-08-01 10:00:00 +0530

    init admin app component

---
+++ admin/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Home from './pages/Home/Home'
+import RestaurantRegistration from "./components/Registrer/RestaurantRegistration";
+import RestaurantLogin from "./components/Login/RestaurantLogin";
+import Dashboard from "./components/Sidebar/Dashboard";
+import LandingPage from "./pages/Home/LandingPage";
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  // Set authentication status after login
+  const handleLogin = () => setIsAuthenticated(true);
+
+  return (
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<RestaurantRegistration />} />
+        <Route path="/login" element={<RestaurantLogin onLogin={handleLogin} />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/dashboard/*"
+          element={
+            isAuthenticated ? <Dashboard /> : <Navigate to="/login" />
+          }
+        />
+      </Routes>
+  );
+};
+
+export default App;
